5edda3b4edd4cb4272c4f1d96227378d8e45bfcb
Chris Lewis <13chlewis@gmail.com>
2018-12-05 09:18:30 -0700

    Created joke_test

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -2,7 +2,6 @@ require 'minitest/autorun'
 require './lib/joke'
 
 class JokeTest < Minitest::Test
-
   def test_it_exists
     joke = Joke.new(1, 'Why did the strawberry cross the road?', 'Because his mother was in a jam.')
     assert_instance_of Joke, joke
